a60af13ce6858e5e823c652b7fe7d16fcd77d87e
omarZACK <o.e.wawy@gmail.com>
2025-03-29 18:43:40 +0300

    final project

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path
-from .views import HelloWorldView
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet,basename='hello-viewset')
+router.register('profile',views.UserProfileViewSet)
+router.register('feed',views.UserProfileFeedViewSet)
+
 urlpatterns =[
-    path('hello-view/',HelloWorldView.as_view())
+    path('hello-view/',views.HelloWorldView.as_view()),
+    path('login/',views.UserLoginApiView.as_view()),
+    path('',include(router.urls))
 ]
